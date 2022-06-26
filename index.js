a4e102f402909e659b4aef4b965dc87808ed33a9
sagarshriwastav <90346552+sagarshriwastav@users.noreply.github.com>
2022-06-26 22:19:39 +0530

    initial upload

---
+++ index.js
@@ -0,0 +1,21 @@
+// alert("working js");
+var randomNumber1 = Math.ceil(Math.random() * 6);//1-6
+var randomDiceImage = "images/dice" + randomNumber1 + ".png";
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomDiceImage);
+
+
+var randomNumber2 = Math.ceil(Math.random() * 6);
+var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+var image2 = document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="Player 1 Wins!🤟💃"
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML="Player 2 Wins!🤟⭐❤️❤️❤️";
+}
+else{
+    document.querySelector("h1").innerHTML="Match Draw👍";
+}
+
